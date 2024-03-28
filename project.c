42d4a320e7e5b25e3a441cf026c26f2512e47007
Logan Brown <158978873+lbrown169@users.noreply.github.com>
2024-03-28 19:55:13 -0400

    Implement instruction_fetch

--- project.c
+++ project.c
@@ -12,7 +12,11 @@ void ALU(unsigned A,unsigned B,char ALUControl,unsigned *ALUresult,char *Zero)
 /* 10 Points */
 int instruction_fetch(unsigned PC,unsigned *Mem,unsigned *instruction)
 {
-
+    if (PC % 4 != 0) {
+        return 1;
+    }
+    *instruction = Mem[PC >> 2];
+    return 0;
 }
 
 
